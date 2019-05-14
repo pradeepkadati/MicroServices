5bc60920169d3ea6bfc81d50bc8c884cd219ca0d
pradeep kadati <pradeepkadati@gmail.com>
2019-05-13 21:15:44 -0700

    01. Initial Commit: Creating Projects and Configurations

---
+++ MovieRatingService/src/main/java/io/kp/api/movierating/MovieRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.kp.api.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingServiceApplication.class, args);
+	}
+
+}
